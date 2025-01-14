5219cd764133751c2cab18b003d7dba4e45eb02e
Andrei Pronsky <95336511+AndreiPronsky@users.noreply.github.com>
2025-01-14 10:40:56 +0300

    Initial commit

---
+++ src/main/java/org/pronsky/securitysandbox/SecuritySandboxApplication.java
@@ -0,0 +1,13 @@
+package org.pronsky.securitysandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pronsky/securitysandbox/SecuritySandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pronsky.securitysandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
