cfbc57015384dcff1f0cea67eb1ad343fecf66a3
Andrei Pronsky <95336511+AndreiPronsky@users.noreply.github.com>
2025-01-14 12:39:21 +0300

    Initial config

--- src/main/java/org/pronsky/securitysandbox/SecuritySandboxApplication.java
+++ src/main/java/org/pronsky/securitysandbox/SecuritySandboxApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecuritySandboxApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecuritySandboxApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritySandboxApplication.class, args);
+    }
 
 }
--- src/test/java/org/pronsky/securitysandbox/SecuritySandboxApplicationTests.java
+++ src/test/java/org/pronsky/securitysandbox/SecuritySandboxApplicationTests.java
@@ -1,13 +1,8 @@
 package org.pronsky.securitysandbox;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SecuritySandboxApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
